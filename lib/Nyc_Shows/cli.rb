85a59bf52c59415d23f92527c3091e04f0b1c5e3
Ashley Wong <ashwong04@gmail.com>
2018-11-27 14:09:56 +0000

    refactor and edits to cli

--- lib/Nyc_Shows/cli.rb
+++ lib/Nyc_Shows/cli.rb
@@ -2,41 +2,44 @@
 class NycShows::CLI
   
   def call
+    NycShows::Scraper.new.make_shows
+    command
+  end
+  
+  def command
     puts "Hello! What would you like to do?"
     puts "Type 'shows' for a list of all show names."
     puts "Type 'broadway' for all broadway shows."
-    puts "Type 'off' for all off-broadway shows."
+    puts "Type 'off-broadway' for all off-broadway shows."
     puts "Type 'story' if you would like to know a play's plot."
     puts "Type 'exit' if you're done for the day."
     input = gets.strip.downcase
       if input == "shows"
         all_shows
-       elsif input == "broadway"
-        broadway
-      elsif input == "off"
-        off_broadway
+       elsif input == "broadway" || input == "off"
+        show_type(input)
       elsif input == "story"
         show_story
       elsif input == "exit"
         puts "See you next time!"
       else
         puts "Please enter a valid command"
-        call
+        command
       end
     end
 
     def all_shows
-      shows = Shows.all.sort {|x,y| x.name <=> y.name}
-      show_list = shows.each_with_index(1) do |show, index|
+      @shows = NycShows::Shows.all
+      @shows = @shows.sort {|x,y| x.name <=> y.name}
+      @shows.each_with_index(1) do |show, index|
         puts "#{index}. #{show.name.titleize} - where: #{show.location} - length: #{show.duration} - genre: #{show.genre}"
       end
-      show_list
     end
     
-    def broadway
+    def show_type(input)
       b_shows = []
-      Shows.all.each do |show|
-        if show.genre == "broadway"
+      @shows.each do |show|
+        if show.genre == input
           b_shows << show
         end
       end
@@ -45,28 +48,13 @@ class NycShows::CLI
         puts "#{index}. #{show.name.titleize}"
       end
       b_list
-      call
-    end
-
-    def off_broadway
-      off_b_shows = []
-      Shows.all.each do |show|
-        if show.genre == "off-broadway"
-          off_b_shows << show
-        end
-      end
-      off_b_shows.sort {|x,y| x.name <=> y.name}
-      off_b_list = off_b_shows.each_with_index(1) do |show, index|
-        puts "#{index}. #{show.name.titleize}"
-      end
-      off_b_list
-      call
+      command
     end
 
     def show_story
       puts "Please enter the name of a show or type 'exit' to return to main menu:"
       s = gets.strip.downcase
-      show = Show.find_by_name(s)
+      show = NycShows::Show.find_by_name(s)
       if show != nil
         puts "#{show.name.titleize} - #{show.story}"
         show_story
@@ -75,7 +63,7 @@ class NycShows::CLI
         show_story
       end
       if s == 'exit'
-        call
+        command
       end
     end
     
