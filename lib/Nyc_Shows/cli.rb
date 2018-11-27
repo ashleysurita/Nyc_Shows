88feeb21297610fc3bb77452280f541624ceb517
Ashley Wong <ashwong04@gmail.com>
2018-11-27 14:58:37 +0000

    changed input into class variable, refactored show_type to map instead of each

--- lib/Nyc_Shows/cli.rb
+++ lib/Nyc_Shows/cli.rb
@@ -8,18 +8,16 @@ class NycShows::CLI
     puts "Type 'story' if you would like to know a play's plot."
     puts "Type 'exit' if you're done for the day."
     
-    input = gets.strip.downcase
-      if input == "shows"
+    @@input = gets.strip.downcase
+      if @@input == "shows"
         all_shows
-       elsif input == "broadway" || input == "off"
-        show_type(input)
-      elsif input == "story"
-        show_story
-      elsif input == "exit"
+       elsif @@input == "broadway" || input == "off"
+        show_type(@@input)
+      elsif @@input == "exit"
         puts "See you next time!"
       else
         puts "Please enter a valid command"
-        command
+        call
       end
     NycShows::Scraper.new.make_shows
     end
@@ -33,18 +31,18 @@ class NycShows::CLI
     end
     
     def show_type(input)
-      b_shows = []
-      @shows.each do |show|
+      @shows = NycShows::Show.all
+      found_shows = @shows.map do |show|
         if show.genre == input
-          b_shows << show
+          show
         end
       end
-      b_shows.sort {|x,y| x.name <=> y.name}
-      b_list = b_shows.each_with_index(1) do |show, index|
-        puts "#{index}. #{show.name.titleize}"
+      found_shows.sort {|x,y| x.name <=> y.name}
+      list = found_shows.each_with_index do |show, index|
+        puts "#{index+1}. #{show.name.titleize}"
       end
-      b_list
-      command
+      list
+      call
     end
 
     def show_story
