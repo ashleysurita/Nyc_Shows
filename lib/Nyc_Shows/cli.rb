720f7ae8ae9fd91d766104215e11d2cea1df46ef
Ashley Wong <ashwong04@gmail.com>
2018-11-28 14:18:56 +0000

    edited errors and separated call and command method in cli

--- lib/Nyc_Shows/cli.rb
+++ lib/Nyc_Shows/cli.rb
@@ -1,25 +1,33 @@
 class NycShows::CLI
   
   def call
+    NycShows::Scraper.new#.make_shows
+    command
+    all_shows
+    show_type
+    show_story
+  end
+  
+  def command
     puts "Hello! What would you like to do?"
     puts "Type 'shows' for a list of all show names."
     puts "Type 'broadway' for all broadway shows."
     puts "Type 'off-broadway' for all off-broadway shows."
     puts "Type 'story' if you would like to know a play's plot."
     puts "Type 'exit' if you're done for the day."
-    
-    @@input = gets.strip.downcase
-      if @@input == "shows"
+   input = gets.strip.downcase
+      if input == "shows"
         all_shows
-       elsif @@input == "broadway" || input == "off"
-        show_type(@@input)
-      elsif @@input == "exit"
+      elsif input == "broadway" || input == "off-broadway"
+        show_type(input)
+      elsif input == "story"
+        show_story
+      elsif input == "exit"
         puts "See you next time!"
       else
         puts "Please enter a valid command"
-        call
+        command
       end
-    NycShows::Scraper.new.make_shows
     end
 
     def all_shows
@@ -37,12 +45,11 @@ class NycShows::CLI
           show
         end
       end
-      found_shows.sort {|x,y| x.name <=> y.name}
-      list = found_shows.each_with_index do |show, index|
+      sorted = found_shows.sort {|x,y| x.name <=> y.name}
+      sorted.each_with_index do |show, index|
         puts "#{index+1}. #{show.name.titleize}"
       end
-      list
-      call
+      command
     end
 
     def show_story
