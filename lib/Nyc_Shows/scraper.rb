e8810d793064d3b325905aeb29b60cab1171eb33
Ashley Wong <ashwong04@gmail.com>
2018-12-03 01:54:07 +0000

    changed add_attr method, moving pry around in show_info method to figure out issue with [:duration]

--- lib/Nyc_Shows/scraper.rb
+++ lib/Nyc_Shows/scraper.rb
@@ -5,14 +5,24 @@ class NycShows::Scraper
     def self.scrape_home_page
       @@pages.each do |page|  
         html = Nokogiri::HTML(open(page))
-        flex = html.css('div.flex-grid.flex-grid--bsrow')
-        grid = flex.css('div.card.card--hover.card--shadow.bg-white.mtn')
+        # flex = html.css('div.flex-grid.flex-grid--bsrow')
+        grid = html.css('div.card.card--hover.card--shadow.bg-white.mtn')
         grid.each do |show|
           name = show.css('div.media-body h2 a').text
           show_url = show.css('div.media-body h2 a.link-111-111').attribute('href').value
-          show_hash = {:name => name.downcase, :show_url => "https://www.broadway.com#{show_url}"}
+          show_hash = {:name => name.downcase, :url => "https://www.broadway.com#{show_url}"}
           NycShows::Show.new(show_hash)
         end
+      end
+    end
+    
+    def self.add_attr
+      self.scrape_home_page.each do |show|
+        info = self.show_info(show)
+        show.add_show_attributes(info)
+        # attr_hash = self.show_info(home_site[:show_url])
+        # show.url = home_site[:show_url]
+        # show.add_show_attributes(attr_hash)
       end
     end
 
@@ -23,19 +33,10 @@ class NycShows::Scraper
       show_info[:story] = html.css('div.gray-dk.inner-content-bold p').text.downcase
       s_bar = html.css('div.ptx')
       show_info[:location] = s_bar.css('a.block.blue-link-lt.lh-norm').text.downcase
-      show_info[:duration] = s_bar.css('div.wht-md')[3].text.downcase
+      binding.pry
+      show_info[:duration] = s_bar.css('div.wht-md')[3].text.downcase #returning empty array
       show_info[:genre] = s_bar.css('li.standard-list__list-item.standard-list__list-item_tighter')[0].text.strip.downcase
       show_info
     end
       
-    def self.add_attr
-      NycShows::Show.all.each do |show|
-        info = self.show_info(show.url)
-        show.add_show_attributes(info)
-        # attr_hash = self.show_info(home_site[:show_url])
-        # show.url = home_site[:show_url]
-        # show.add_show_attributes(attr_hash)
-      end
-    end
-
 end
