2d6b2ef8770577c08273381f6ce9f897e35689ec
Ashley Wong <ashwong04@gmail.com>
2018-12-11 01:07:17 +0000

    added show_number method

--- lib/Nyc_Shows/cli.rb
+++ lib/Nyc_Shows/cli.rb
@@ -9,6 +9,7 @@ class NycShows::CLI
   def command
     puts "What would you like to do?"
     puts "Type 'shows' for a list of all show names."
+    puts "Type 'number' to see a show by it's number."
     puts "Type 'broadway' for all broadway shows."
     puts "Type 'off-broadway' for all off-broadway shows."
     puts "Type 'story' if you would like to know a play's plot."
@@ -18,6 +19,8 @@ class NycShows::CLI
      case input
        when "shows"
         all_shows
+        when "number"
+          show_number
        when "broadway"
          show_type(input)
        when "off-broadway"
@@ -37,6 +40,20 @@ class NycShows::CLI
       @shows = @shows.sort {|x,y| x.name <=> y.name}
       @shows.each_with_index do |show, index|
         puts "#{index+1}. #{show.name.capitalize} - Theater: #{show.location.capitalize} - Show length: #{show.duration} - Genre: #{show.genre.capitalize}"
+      end
+      command
+    end
+    
+    def show_number
+      shows = NycShows::Show.all
+      puts "Enter a show number from the complete list or type 'exit' to return to the main menu:"
+      input = gets.strip.to_i
+      case input
+        when 'exit'
+          command
+        else  
+          all_list = shows.sort {|x,y| x.name <=> y.name}
+          puts "#{all_list[input-1].name}"
       end
       command
     end
